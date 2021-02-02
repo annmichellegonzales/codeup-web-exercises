fda4782ef8901012b712979adf56575d1ba2d130
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-02-02 13:50:47 -0600

    completed jquery

--- jquery-exercises.js
+++ jquery-exercises.js
@@ -14,6 +14,28 @@
 //     alert("I gotta run I gotta run");
 // });
 
-$(document).ready(function () {
-    alert("The DOM has finished loading.");
-});
+// $(document).ready(function () {
+//     alert("The DOM has finished loading.");
+// });
+
+
+
+// $('#para-id').css('background-color', 'purple');
+// $('#divvy').css('background-color', 'red');
+
+
+
+// $('.codeup').css('background-color', 'yellow');
+// $('.codeup').css('border', '1px solid #F00');
+
+//
+// $('li').css('font-size', '20px');
+// $('h1').css('border', '2px dotted blue');
+//
+// $('.ele-class').css('background-color', 'pink');
+
+// $('h1').css('color', 'purple');
+// $('p').css('color', 'red');
+// $('li').css('color', 'gray');
+
+// $('h1, p, li').css('color', 'green');
