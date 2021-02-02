5820380ffe0c80a51e12931c894ea561a9681a99
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-02-02 15:45:53 -0600

    alerts and h1's

--- jquery-exercises.js
+++ jquery-exercises.js
@@ -23,7 +23,9 @@
 // $('#para-id').css('background-color', 'purple');
 // $('#divvy').css('background-color', 'red');
 
-
+// $(document).ready(function () {
+//     alert("The paragraph and div bg color changed.");
+// });
 
 // $('.codeup').css('background-color', 'yellow');
 // $('.codeup').css('border', '1px solid #F00');
@@ -34,8 +36,16 @@
 //
 // $('.ele-class').css('background-color', 'pink');
 
-// $('h1').css('color', 'purple');
-// $('p').css('color', 'red');
-// $('li').css('color', 'gray');
+// $('h1').css('background-color', 'purple');
+// $('p').css('background-color', 'red');
+// $('li').css('background-color', 'gray');
 
 // $('h1, p, li').css('color', 'green');
+
+// var contents = $('#codeup').html();
+// alert(contents);
+
+// alert($('#codeup').html());
+$('h1').each(function() {
+    alert($(this).html());
+})
