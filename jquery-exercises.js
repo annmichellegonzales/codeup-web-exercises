13626e56678603699c7f84629a72d78066115bc0
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-02-02 10:52:01 -0600

    jquery on new branch

---
+++ jquery-exercises.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// window.onload = function () {
+//     alert("Ding, fries are done!");
+// }
+
+// console.log("test");
+// console.log(jQuery);
+// console.log($);
+// $
+
+
+// $(document).ready(function() {
+//     alert("I gotta run I gotta run");
+// });
+
+$(document).ready(function () {
+    alert("The DOM has finished loading.");
+});
