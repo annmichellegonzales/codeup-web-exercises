d4dd3001aab5ef3cb9c140a94af750c5ab9d0d7a
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-02-11 17:52:26 -0600

    establishing variables lat/lng

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,8 +1,9 @@
-displayContent(29.4241, -98.4936);
+let initLat = 29.4241;
+let initLng = -98.4936;
+displayContent(initLat, initLng);
 
+// retrieves information from the api, uses personal token
 function displayContent(lat, lng) {
-
-
     $.get("https://api.openweathermap.org/data/2.5/onecall", {
         APPID: OWM_TOKEN,
         lat: lat,
@@ -11,60 +12,85 @@ function displayContent(lat, lng) {
         exclude: "minutely, hourly",
     }).done(function (data) {
         console.log(data);
-        let day = data.daily;
-        let html = '';
-        for (let i = 0; i < 5; i++) {
-            console.log(day[i]);
-            // let icon = a.weather[0].icon;
-            let unixTime = day[i].dt;
-            let date = new Date(unixTime * 1000);
-            let date1 = date.toLocaleDateString("en-US");
-            let dayHtml = "<div class='card' style='width: 18rem'>";
-            dayHtml += "<div class='card-header'>" + date1 + "</div>";
-            dayHtml += "<ul class='list-group list-group-flush'>";
-            dayHtml += "<li class='list-group-item text-center'>" + "<img src='https://openweathermap.org/img/w/10d.png'" + "</li>";
-            dayHtml += "<li class='list-group-item text-center'>" + "High Temperature: " + day[i].temp.max + "℉" + "</li>";
-            dayHtml += "<li class='list-group-item text-center'>" + "Low Temperature: " + day[i].temp.min + "℉" + "</li>";
-            dayHtml += "</ul>";
-            dayHtml += "</div>";
-            html += dayHtml;
-        }
-        $("#weather").html(html);
-        // $('#wicon').attr('src', iconurl);
+        displayWeather(data);
     });
 }
 
+// creates display for html using the weather id
+function displayWeather(data) {
+    let day = data.daily;
+    let html = '';
+    for (let i = 0; i < 5; i++) {
+        console.log(day[i]);
+        let weatherIcon = day[i].weather[0].icon;
+        let unixTime = day[i].dt;
+        let date = new Date(unixTime * 1000);
+        let date1 = date.toLocaleDateString("en-US");
+        let dayHtml = "<div class='card' style='width: 18rem'>";
+        dayHtml += "<div class='card-header'>" + date1 + "</div>";
+        dayHtml += "<ul class='list-group list-group-flush'>";
+        dayHtml += "<li class='list-group-item text-center'>" + "<img src='https://openweathermap.org/img/w/" + weatherIcon + ".png'" + "</li>";
+        dayHtml += "<li class='list-group-item text-center'>" + "High Temperature: " + day[i].temp.max + "℉" + "</li>";
+        dayHtml += "<li class='list-group-item text-center'>" + "Low Temperature: " + day[i].temp.min + "℉" + "</li>";
+        dayHtml += "</ul>";
+        dayHtml += "</div>";
+        html += dayHtml;
+    }
+    $("#weather").html(html);
+}
+
+
+// displays map to page
 mapboxgl.accessToken = MAPBOX_TOKEN;
-var coordinates = document.getElementById('coordinates');
+var coordinates = document.getElementById('coordinates'); //uses entered coordinates
 var map = new mapboxgl.Map({
-    container: 'mapbox-map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-98.49, 29.42], // starting position [lng, lat]
-    zoom: 10 // starting zoom
+    container: 'mapbox-map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', //stylesheet location
+    center: [-98.49, 29.42], // starting position
+    zoom: 9 // starting zoom
 });
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
+
+// adding marker to map, able to drag
 var marker = new mapboxgl.Marker({
     draggable: true
 })
-    .setLngLat([-98.435697, 29.373502])
+    .setLngLat([initLng, initLat])
     .addTo(map);
+
+
+
+
+
+function onDragEnd() {
+    var lngLat = marker.getLngLat();
+    lng = lngLat.lng;
+    lat = lngLat.lat;
+    reverseGeocode(lngLat, MAPBOX_TOKEN).then(function (result){
+        input.val(result);
+    })
+    displayContent();
+}
+
+marker.on('dragend', onDragEnd);
+
 
 $("#button").click(function (e) {
     e.preventDefault();
-    var message = $("#type-in").val()
-    geocode(message, MAPBOX_TOKEN).then(function (data) {
+    geocode($("#input").val(), MAPBOX_TOKEN).then(function (data) {
+        displayWeather(data[0], data[1]);
         map.flyTo({
-            center: [data[1], data[0]],
+            center: [data[0], data[1]],
             essential: true,
             zoom: 5
-        })
-        marker.setLngLat([data[1], [data][0]]);
+        });
+        marker.setLngLat([data[0], [data][1]]);
         marker.addTo(map);
     })
-})
+});
 
 function geocoderEventPrep(e) {
     if (e.target.className.indexOf('mapboxgl-ctrl-geocoder--input') !== -1) {
@@ -79,14 +105,13 @@ function geocoderEventPrep(e) {
         });
     }
 }
-
+//
 document.addEventListener('change', geocoderEventPrep);
-
-function onDragEnd() {
-    var lngLat = marker.getLngLat();
-    coordinates.style.display = 'block';
-    coordinates.innerHTML =
-        'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-}
-
-marker.on('dragend', onDragEnd);
+//
+// function onDragEnd() {
+//     var lngLat = marker.getLngLat();
+//     coordinates.style.display = 'block';
+//     coordinates.innerHTML =
+//         'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+// }
+//
