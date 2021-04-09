35565a6884ecbf5d09112f4bd2783230f042f194
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-04-09 18:05:47 -0500

    refactored date on weather map

--- js/weather-map.js
+++ js/weather-map.js
@@ -28,7 +28,7 @@ function displayWeather(data) {
         let weatherIcon = day[i].weather[0].icon;
         let unixTime = day[i].dt;
         let date = new Date(unixTime * 1000);
-        let date1 = date.toLocaleDateString("en-US");
+        let date1 = date.toLocaleDateString("en-US", { weekday: 'long' });
         let dayHtml = "<div class='card' style='width: 18rem'>";
         dayHtml += "<div class='card-header'>" + date1 + "</div>";
         dayHtml += "<ul class='list-group list-group-flush'>";
@@ -56,80 +56,80 @@ let map = new mapboxgl.Map({
 
 
 
+//
+//
+//
+// // Add zoom and rotation controls to the map.
+// map.addControl(new mapboxgl.NavigationControl());
+//
+//
+// // adding marker to map, able to drag
+// let marker = new mapboxgl.Marker({
+//     draggable: true
+// })
+//     // .setLngLat([initLng, initLat]);
+//     // .addTo(map);
+//
+//
 
 
 
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
 
-
-// adding marker to map, able to drag
-let marker = new mapboxgl.Marker({
-    draggable: true
-})
-    // .setLngLat([initLng, initLat]);
-    // .addTo(map);
-
-
-
-
-
-
-
-let coordinatesGeocoder = function (query) {
-// Match anything which looks like
-// decimal degrees coordinate pair.
-    let matches = query.match(
-        /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
-    );
-    if (!matches) {
-        return null;
-    }
-
-    function coordinateFeature(lng, lat) {
-        return {
-            center: [lng, lat],
-            geometry: {
-                type: 'Point',
-                coordinates: [lng, lat]
-            },
-            place_name: 'Lat: ' + lat + ' Lng: ' + lng,
-            place_type: ['coordinate'],
-            properties: {},
-            type: 'Feature'
-        };
-    }
-
-    let coord1 = Number(matches[1]);
-    let coord2 = Number(matches[2]);
-    let geocodes = [];
-
-    if (coord1 < -90 || coord1 > 90) {
-// must be lng, lat
-        geocodes.push(coordinateFeature(coord1, coord2));
-    }
-
-    if (coord2 < -90 || coord2 > 90) {
-// must be lat, lng
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-
-    if (geocodes.length === 0) {
-// else could be either lng, lat or lat, lng
-        geocodes.push(coordinateFeature(coord1, coord2));
-        geocodes.push(coordinateFeature(coord2, coord1));
-    }
-
-    return geocodes;
-};
-
-// Add the control to the map.
-map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        localGeocoder: coordinatesGeocoder,
-        zoom: 4,
-        placeholder: 'Try: -40, 170',
-        mapboxgl: mapboxgl
-    })
-);
+//
+// let coordinatesGeocoder = function (query) {
+// // Match anything which looks like
+// // decimal degrees coordinate pair.
+//     let matches = query.match(
+//         /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+//     );
+//     if (!matches) {
+//         return null;
+//     }
+//
+//     function coordinateFeature(lng, lat) {
+//         return {
+//             center: [lng, lat],
+//             geometry: {
+//                 type: 'Point',
+//                 coordinates: [lng, lat]
+//             },
+//             place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+//             place_type: ['coordinate'],
+//             properties: {},
+//             type: 'Feature'
+//         };
+//     }
+//
+//     let coord1 = Number(matches[1]);
+//     let coord2 = Number(matches[2]);
+//     let geocodes = [];
+//
+//     if (coord1 < -90 || coord1 > 90) {
+// // must be lng, lat
+//         geocodes.push(coordinateFeature(coord1, coord2));
+//     }
+//
+//     if (coord2 < -90 || coord2 > 90) {
+// // must be lat, lng
+//         geocodes.push(coordinateFeature(coord2, coord1));
+//     }
+//
+//     if (geocodes.length === 0) {
+// // else could be either lng, lat or lat, lng
+//         geocodes.push(coordinateFeature(coord1, coord2));
+//         geocodes.push(coordinateFeature(coord2, coord1));
+//     }
+//
+//     return geocodes;
+// };
+//
+// // Add the control to the map.
+// map.addControl(
+//     new MapboxGeocoder({
+//         accessToken: mapboxgl.accessToken,
+//         localGeocoder: coordinatesGeocoder,
+//         zoom: 4,
+//         placeholder: 'Try: -40, 170',
+//         mapboxgl: mapboxgl
+//     })
+// );
